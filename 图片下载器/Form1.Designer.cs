849b3aaecb666e0467071c66f615792ae1268243
HuoDa <huoda2008@163.com>
2013-12-27 21:06:04 +0800

    commit first

---
+++ 图片下载器/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace 图片下载器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSourceUrl = new System.Windows.Forms.TextBox();
+            this.txtDesUrl = new System.Windows.Forms.TextBox();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "网页地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "保存地址：";
+            // 
+            // txtSourceUrl
+            // 
+            this.txtSourceUrl.Location = new System.Drawing.Point(164, 71);
+            this.txtSourceUrl.Name = "txtSourceUrl";
+            this.txtSourceUrl.Size = new System.Drawing.Size(263, 21);
+            this.txtSourceUrl.TabIndex = 2;
+            // 
+            // txtDesUrl
+            // 
+            this.txtDesUrl.Location = new System.Drawing.Point(164, 170);
+            this.txtDesUrl.Name = "txtDesUrl";
+            this.txtDesUrl.Size = new System.Drawing.Size(263, 21);
+            this.txtDesUrl.TabIndex = 3;
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(219, 241);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 4;
+            this.btnDownLoad.Text = "下载";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(433, 168);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "浏览";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(164, 123);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(263, 21);
+            this.txtRegex.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "正则表达式：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 320);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.txtDesUrl);
+            this.Controls.Add(this.txtSourceUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "图片下载器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSourceUrl;
+        private System.Windows.Forms.TextBox txtDesUrl;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 图片下载器/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace 图片下载器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownLoad_Click(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtDesUrl.Text, @"\w{1}:(\\?\S*)+"))
+            {
+                MessageBox.Show("保存路径设置错误，请重新设置！");
+                return;
+            }
+            WebClient wc = new WebClient();
+            wc.Encoding = System.Text.Encoding.UTF8;//设置抓取源代码为utf-8编码
+            string content = "";
+            try
+            {
+                content = wc.DownloadString(txtSourceUrl.Text);
+            }
+            catch (Exception)
+            {
+                //btnDownLoad_Click(null, null);
+                MessageBox.Show("网络连接错误，请重新下载！");
+                return;
+            }
+            //百度图片正则表达式
+            //string pattern = @"thumbUR[\s\S]+?objURL[^\s]+?(?<url>[a-zA-Z]+://[^\s]*?.jpg)";
+            //百度空间图片正则表达式
+            //string pattern = @"<p>(?<name>\w+)&shy;</p><p>&shy;</p><p><img\salt=""图片""\s+src=""(?<url>\w+://[\w.?/?]+.jpg)""";
+            string pattern = txtRegex.Text;
+            MatchCollection matchs = Regex.Matches(content, pattern);
+            if (DownLoad(matchs, txtDesUrl.Text))
+            {
+                MessageBox.Show("下载完成！");
+            }
+            else
+            {
+                MessageBox.Show("下载失败！");
+            }
+        }
+
+        private static bool DownLoad(MatchCollection matchs,string path)
+        {
+            WebClient wc = new WebClient();
+            int count = 0;
+            foreach (Match match in matchs)
+            {
+                string strUrl = match.Groups["url"].Value;
+                string name = string.Format("{0}\\{1}.jpg", path, match.Groups["name"].Value);
+                try
+                {
+                    wc.DownloadFile(strUrl, name);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                count++;
+            }
+            wc.Dispose();
+            return true;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            txtDesUrl.Text = fbd.SelectedPath;
+        }
+
+    }
+}
